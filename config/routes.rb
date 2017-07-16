847476b0e3c545de9dbc8a0736967499099066ae
Tim Schulte <tschult0@chicagobooth.edu>
2017-07-16 13:15:47 +0000

    zodiac_attempt_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,30 @@
 Rails.application.routes.draw do
-  get("/",        { :controller => "fortunes", :action => "lucky" })
-
-  get("/lucky_numbers",   { :controller => "fortunes", :action => "lucky" })
-  get("/unlucky_numbers", { :controller => "fortunes", :action => "unlucky" })
+  get("/",                    { :controller => "fortunes", :action => "lucky" })
+  get("/lucky_numbers",       { :controller => "fortunes", :action => "lucky" })
+  get("/unlucky_numbers",     { :controller => "fortunes", :action => "unlucky" })
+  get("/zodiacs/aries",       { :controller =>  "zodiacs", :action =>  "ram" })
+  get("/zodiacs/taurus",      { :controller =>  "zodiacs", :action =>  "bull" })
+  get("/zodiacs/gemini",      { :controller =>  "zodiacs", :action => "twins" })
+  get("/zodiacs/cancer",      { :controller =>  "zodiacs", :action =>  "crab" })
+  get("/zodiacs/leo",         { :controller =>  "zodiacs", :action =>  "lion" })
+  get("/zodiacs/virgo",       { :controller =>  "zodiacs", :action =>  "maiden" })
+  get("/zodiacs/libra",       { :controller =>  "zodiacs", :action =>  "scales" })
+  get("/zodiacs/scorpio",     { :controller =>  "zodiacs", :action =>  "scorpion" })
+  get("/zodiacs/sagittarius", { :controller =>  "zodiacs", :action =>  "archer" })
+  get("/zodiacs/capricorn",   { :controller =>  "zodiacs", :action =>  "goat" })
+  get("/zodiacs/aquarius",    { :controller =>  "zodiacs", :action =>  "waterbearer" })
+  get("/zodiacs/pisces",      { :controller =>  "zodiacs", :action =>  "fish" })
 
   # PART 1: STATIC ROUTES
   # =====================
 
   # Uncomment each route below ONE AT A TIME and debug.
-
-  # get("/zodiacs/aries",       { :controller =>  zodiacs,  action =>  "ram" })
-  # get("/zodiacs/taurus",      { :controller,   "zodiacs", :action,   "bull" })
-  # get(/zodiacs/gemini,        { :controller => "zodiacs", :action => "twins" })
-  # get("/zodiacs/cancer"       { :controller => "zodiacs"  :action => "crab" })
-  # get ("/zodiacs/leo",        { :controller => "zodiac",  :action => "lion" })
-  # get(" /zodiacs/virgo",      { :controller => "Zodiacs", :action => "maiden" })
-  # get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
-  # get("/zodiac/scorpio",      { :controller => "zodiacs", :action => "scorpion" })
-  # get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
-  # get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
-  # get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
-  # get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
+  
+  
+  
+  
+  
+  
 
   mount WebGit::Engine, at: "/rails/git"
 end
